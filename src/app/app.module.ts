66a3ff83300748fca63536f057b28e418cf18413
haholl <haholl@taltech.ee>
2024-11-16 18:21:39 +0200

    Updated style, added scrollTo

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AppRoutingModule } from './app.routes';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule,
+    NavbarComponent,
+    AppRoutingModule
+  ],
+  providers: []
+})
+export class AppModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
+
